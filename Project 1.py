d820e4b9ae7e29c7d2e8fe5de5556e77f9c045fb
JosephBal <121900071+JosephBal@users.noreply.github.com>
2024-07-31 07:32:49 -0400

    Add files via upload

---
+++ Project 1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  9 14:21:57 2023
+
+@author: Admin
+"""
+
+# -*- coding: utf-8 -*-
+# Email: joseph.Balbuena@baruchmail.cuny.edu
+# File: Joseph_homework_1.py
+# Purpose: Sample code to help you get started on the Project
+
+# define variables for letter values that I need:
+o = 1
+r = 1
+a = 1
+n = 1
+g = 2
+e = 1
+s = 1
+# calculate (compute) the value for : 'oranges'
+value = o+r+a+n+g+e+s
+
+# print out the value
+print(value)
+# value is 8
